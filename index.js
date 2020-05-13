857ead78ea4a4da7fd1610f3d5559b25b3379edf
swsw121333 <swsw1213@nate.com>
2020-05-13 14:21:04 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'process.env.token';
+const token = process.env.token;
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
